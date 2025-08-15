2c58c32f1d073c5d697470a4808f27c57158a6f3
Maulana Ikhsan <anjay1435@gmail.com>
2025-08-15 15:43:19 +0700

    Menambahkan Endpoint Ruangan

--- routes/akademik/ruangan.router.js
+++ routes/akademik/ruangan.router.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 // import { validateCreatePeriodeAkademik } from "../../validators/periode-akademik.validators.js";
 import * as RuanganController from "../../controllers/akademik/ruangan.controller.js";
+import { validateCreateRuangan } from "../../validators/ruangan.validator.js";
 const router = new Router();
 
 router.get("/", RuanganController.findAll);
+router.get("/", validateCreateRuangan, RuanganController.create);
 
 export default router;
