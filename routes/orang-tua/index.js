d69a1b07714075c7552f51afad6ccb1a6a0a133c
Muhammad Syaifullah Nurrahman <31509479+IipulI@users.noreply.github.com>
2026-05-15 13:55:10 +0700

    Update fitur ipk

--- routes/orang-tua/index.js
+++ routes/orang-tua/index.js
@@ -1,18 +1,11 @@
 import { Router } from "express";
 import { getWeeklySchedule } from "../../controllers/orang-tua/jadwal-akademik.controller.js";
-<<<<<<< HEAD
-import { getHasilStudi } from "../../controllers/orang-tua/hasil-studi.controller.js";
-=======
 import { getHasilStudi, getIpk } from "../../controllers/orang-tua/hasil-studi.controller.js";
->>>>>>> ea1c904 (Menambah fitur get ipk untuk modul orang tua)
 
 const router = Router();
 
 router.get('/jadwal-akademik/minggu', getWeeklySchedule)
 router.get('/hasil-studi', getHasilStudi)
-<<<<<<< HEAD
-=======
 router.get('/hasil-studi/ipk', getIpk)
->>>>>>> ea1c904 (Menambah fitur get ipk untuk modul orang tua)
 
 export default router;
